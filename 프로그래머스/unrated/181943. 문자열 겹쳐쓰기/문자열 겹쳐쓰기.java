621fd1164c5e50920a1f8ac7af585d79ceadadd8
js1171 <149784847+js1171@users.noreply.github.com>
2023-11-06 20:50:06 +0900

    [level unrated] Title: 문자열 겹쳐쓰기, Time: 0.04 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,12 +1,8 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        String answer = "";
-        
-        String first = my_string.substring(0, s);
-        String last = my_string.substring(s + overwrite_string.length());
-    
-        answer = first + overwrite_string + last;
-    return answer;
+        StringBuilder sb = new StringBuilder(my_string);
+        sb.replace(s,overwrite_string.length()+s,overwrite_string);
+    return sb.toString();
     }
     
 }
