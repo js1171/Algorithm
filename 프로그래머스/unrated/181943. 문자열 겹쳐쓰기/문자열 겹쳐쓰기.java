d3c21978b7d4241b75c66641019f20431a59c240
js1171 <149784847+js1171@users.noreply.github.com>
2023-11-06 22:00:22 +0900

    Update 문자열 겹쳐쓰기.java
    설명 추가

--- 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/unrated/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,8 +1,8 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
-        StringBuilder sb = new StringBuilder(my_string);
-        sb.replace(s,overwrite_string.length()+s,overwrite_string);
-    return sb.toString();
+        StringBuilder sb = new StringBuilder(my_string);                                  // StringBuilder 선언하고 my_string 넣어주기
+        sb.replace(s,overwrite_string.length()+s,overwrite_string);                       // s부터 s+overwrite_string길이 까지 overwrite_string으로 대체
+    return sb.toString();                                                                 // String으로 변환
     }
     
 }
