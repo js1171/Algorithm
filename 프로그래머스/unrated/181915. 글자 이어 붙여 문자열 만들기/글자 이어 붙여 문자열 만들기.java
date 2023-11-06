f2ea7c3a98fea5325b70f12a44ed79cf00dcc478
js1171 <149784847+js1171@users.noreply.github.com>
2023-11-06 16:29:09 +0900

    Update 글자 이어 붙여 문자열 만들기.java
    설명 추가

--- 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
+++ 프로그래머스/unrated/181915. 글자 이어 붙여 문자열 만들기/글자 이어 붙여 문자열 만들기.java
@@ -8,3 +8,23 @@ class Solution {
         return sb.toString();
     }
 }
+
+
+
+
+
+//다른 사람의 풀이
+for (int 변수 : 리스트)로 사용이 가능한듯!!
+    
+
+class Solution {
+    public String solution(String my_string, int[] index_list) {
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int idx : index_list)
+            sb.append(my_string.charAt(idx));
+
+        return sb.toString();
+    }
+}
