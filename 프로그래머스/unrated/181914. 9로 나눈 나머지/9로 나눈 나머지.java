7ca3ea31e3c93a5bee8ac3d8da00211a01d34130
js1171 <149784847+js1171@users.noreply.github.com>
2023-11-06 16:21:40 +0900

    Update 9로 나눈 나머지.java
    설명 추가

--- 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
+++ 프로그래머스/unrated/181914. 9로 나눈 나머지/9로 나눈 나머지.java
@@ -8,3 +8,9 @@ class Solution {
         return sum%9;
     }
 }
+
+
+number 문자열에서 숫자 하나씩을 charAt으로 가져옴 → 하나하나 Char 형태가 됨
+문자를 숫자 형태로 바꿔서 sum 변수에 저장
+    
+Character.getNumbericvValue(cha) : 주어진 문자의 숫자 값을 반환하는 메소드
