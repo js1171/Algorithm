140d5ee7197dbf14f707550c7872b444a3bbaaea
js1171 <149784847+js1171@users.noreply.github.com>
2023-11-08 16:51:08 +0900

    [level unrated] Title: 접미사 배열, Time: 0.26 ms, Memory: 71.2 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181909. 접미사 배열/접미사 배열.java
@@ -0,0 +1,13 @@
+import java.util.Arrays;
+
+class Solution {
+    public String[] solution(String my_string) {
+        String[] arr = new String[my_string.length()];
+        
+        for (int i=0 ; i < my_string.length() ; i++) {
+            arr[i] = my_string.substring(i);
+        }
+        Arrays.sort(arr);
+        return arr;
+    }
+}
