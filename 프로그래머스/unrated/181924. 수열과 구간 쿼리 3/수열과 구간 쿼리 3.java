e2dbf9703b0e75e36f8dcca4d108f7f2915dbba3
js1171 <149784847+js1171@users.noreply.github.com>
2023-11-09 19:05:33 +0900

    [level unrated] Title: 수열과 구간 쿼리 3, Time: 0.04 ms, Memory: 80.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
+++ 프로그래머스/unrated/181924. 수열과 구간 쿼리 3/수열과 구간 쿼리 3.java
@@ -1,14 +1,12 @@
 class Solution {
     public int[] solution(int[] arr, int[][] queries) {
-        int ci=0, cj=0, i=0, j=0;
+        int ci=0, cj=0;
         
         for (int qi = 0 ; qi < queries.length ; qi++){
-                i = queries[qi][0];
-                j = queries[qi][1];
-                ci = arr[i] ;
-                cj = arr[j] ;
-                arr[i] = cj;
-                arr[j] = ci;
+                ci = arr[queries[qi][0]] ;
+                cj = arr[queries[qi][1]] ;
+                arr[queries[qi][0]] = cj;
+                arr[queries[qi][1]] = ci;
         }
         return arr;    
     }
