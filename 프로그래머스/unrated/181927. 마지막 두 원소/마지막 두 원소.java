b410938f2cbf6076c8827bf23dc7ec9aa92bb0ba
js1171 <149784847+js1171@users.noreply.github.com>
2023-11-09 18:38:31 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.02 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.java
@@ -1,12 +1,11 @@
+import java.util.Arrays;
+
 class Solution {
     public int[] solution(int[] num_list) {
-        int[] list = new int[(num_list.length)+1];
         int n = num_list.length ;
-        int a=num_list[n-1], b=num_list[n-2] ;
-        
-        for (int i=0 ; i<n ; i++) {
-            list[i] = num_list[i];
-        }
+        int a = num_list[n-1], b = num_list[n-2] ;
+
+        int[] list = Arrays.copyOf(num_list, n+1);
         
         list[n] = a > b ? a-b : a*2 ;
         return list;
