b4335cd0238471c7983053638a1957f15a828237
js1171 <149784847+js1171@users.noreply.github.com>
2023-11-23 23:11:58 +0900

    [level unrated] Title: 특정 문자 제거하기, Time: 0.05 ms, Memory: 69.3 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120826. 특정 문자 제거하기/특정 문자 제거하기.java
@@ -0,0 +1,10 @@
+class Solution {
+    public String solution(String my_string, String letter) {
+        StringBuilder sb = new StringBuilder(my_string);
+        
+        while ( sb.indexOf(letter) != -1) {
+            sb.deleteCharAt(sb.indexOf(letter));
+        }
+        return sb.toString();
+    }
+}
