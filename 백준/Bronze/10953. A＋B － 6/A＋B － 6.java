00716ad730cf15557d9544e1336b61a6fe3febfd
js1171 <149784847+js1171@users.noreply.github.com>
2023-12-19 12:04:41 +0900

    [Bronze III] Title: A+B - 6, Time: 128 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+
+        int count = Integer.parseInt(br.readLine());
+
+        for (int i=1 ; i <= count ; i++) {
+            st = new StringTokenizer(br.readLine(),",");
+            int num = Integer.parseInt(st.nextToken()) + Integer.parseInt(st.nextToken());
+            sb.append(num).append("\n");
+        }
+        System.out.println(sb);
+    }
+}
