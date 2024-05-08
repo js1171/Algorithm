5a3c458182bf0997468ff3c34078289c1614edc7
이지선 <149784847+js1171@users.noreply.github.com>
2024-05-08 22:35:21 +0900

    [D4] Title: [S/W 문제해결 기본] 2일차 - Ladder1, Time: 139 ms, Memory: 37,452 KB -BaekjoonHub

---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/README.md
@@ -0,0 +1,15 @@
+# [D4] [S/W 문제해결 기본] 2일차 - Ladder1 - 1210 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AV14ABYKADACFAYh) 
+
+### 성능 요약
+
+메모리: 37,452 KB, 시간: 139 ms, 코드길이: 1,699 Bytes
+
+### 제출 일자
+
+2024-05-08 22:35
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D4/1210. ［S／W 문제해결 기본］ 2일차 － Ladder1/［S／W 문제해결 기본］ 2일차 － Ladder1.java
@@ -0,0 +1,56 @@
+import java.util.*;
+import java.io.*;
+
+class Solution {
+        static int[][] arr;
+    static int result;
+
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        for (int tc = 1 ; tc <= 10 ; tc++) {
+            int caseN = Integer.parseInt(br.readLine());
+
+            // arr에 입력 값 저장
+            arr = new int[100][100];
+            for (int i = 0 ; i < 100 ; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                for (int j = 0 ; j < 100 ; j++) {
+                    arr[i][j] = Integer.parseInt(st.nextToken());
+                }
+            }
+
+            // 마지막 줄에서 2인 곳의 위치 구하기
+            int endIndex = 0;
+            for (int i = 0 ; i < 100 ; i++) {
+                if (arr[99][i] == 2) {
+                    endIndex = i;
+                }
+            }
+            // 올라가면서 시작점 구하기
+            findStartIndex(99, endIndex);
+            sb.append("#").append(caseN).append(" ").append(result).append("\n");
+        }
+        System.out.print(sb);
+    }
+
+    static void findStartIndex(int x, int y) {
+        if (x == 0) {
+            result = y;
+            return;
+        }
+
+        // 좌, 우, 위쪽 순서로 점검하여 이동
+        if (y - 1 >= 0 && arr[x][y - 1] == 1) {
+            arr[x][y - 1] = 2;
+            findStartIndex(x, y - 1);
+        } else if (y + 1 < 100 && arr[x][y + 1] == 1) {
+            arr[x][y + 1] = 2;
+            findStartIndex(x, y + 1);
+        } else {
+            arr[x - 1][y] = 2;
+            findStartIndex(x - 1, y);
+        }
+    }
+}
